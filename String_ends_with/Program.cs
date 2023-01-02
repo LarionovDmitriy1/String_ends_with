b6b92ca07d2215e685f5876197ec5221eabe4641
Дима Ларионов <dima_larionov_000@mail.ru>
2023-01-02 23:39:44 +0300

    Добавьте файлы проекта.

---
+++ String_ends_with/Program.cs
@@ -0,0 +1,4 @@
+﻿using String_ends_with;
+
+var result = StringEndsWith.Solution("jora","ra");
+Console.WriteLine(result);
---
+++ String_ends_with/StringEndsWith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_ends_with
+{
+    public class StringEndsWith
+    {
+        public static bool Solution(string str, string ending)
+        {
+            if (str.Length < ending.Length) { return false; }
+            for (int i = 1; i < ending.Length + 1; i++)
+            {
+                string s = str.Substring(str.Length - i);
+                string s1 = ending.Substring(ending.Length - i);
+
+                if (s == s1)
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
